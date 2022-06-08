b341ffeb00584582fd5d1ff8160b7862468417d3
Thomas Manica <thomas.manica25@gmail.com>
2022-06-07 21:11:13 -0300

    Adiciona tela Splash

--- src/Main/LoginMain.java
+++ src/Main/LoginMain.java
@@ -1,7 +1,10 @@
 package Main;
 
 import Apresentacao.TelaLogin;
+import Apresentacao.TelaSplash;
 import Persistencia.PrimeiraConfiguracaoBancoDeDados;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 public class LoginMain {
@@ -10,6 +13,15 @@ public class LoginMain {
         if (!PrimeiraConfiguracaoBancoDeDados.CriarConfig()) {
             System.exit(0);
         }
+        JFrame telaSplash = new TelaSplash();
+        telaSplash.setVisible(true);
+        try {  
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(LoginMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        telaSplash.setVisible(false);
+        
         JFrame telaInicial = new TelaLogin();
         telaInicial.setVisible(true);
     }
